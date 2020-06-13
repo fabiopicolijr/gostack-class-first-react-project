495bd2641981a9636a082c9fa015e158ef251af7
Fabio Picoli Jr <fabiopicolijr@gmail.com>
2020-06-13 13:43:41 -0300

    config

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// const Dashboard = () => {} === isso é igual a function Dashboard() {}
+// React.FC === React.functioncomponent
+
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
